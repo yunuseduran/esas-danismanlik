4ca19a408d63e26cb081a8150cf07a4847482503
Yunus Emre Duran <iamyunusemreduran@gmail.com>
2025-11-19 10:56:59 +0300

    manifest.ts changes

--- app/manifest.ts
+++ app/manifest.ts
@@ -14,7 +14,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/esas-logo.png',
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'any maskable',
+        purpose: 'any',
+      },
+      {
+        src: '/esas-logo.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
       },
     ],
   };
